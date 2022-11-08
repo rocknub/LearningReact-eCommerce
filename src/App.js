fca9257d3d1a498f3a6b96e899b76bf7f08a1398
Brian Mendes <brian.mendes.f@gmail.com>
2022-11-08 12:10:31 -0300

    Front-end completed.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Checkout from './components/Checkout/Checkout';
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    //Basic app starter page.
     <div className="App">
       <Router>
+
         <Switch>
 
-          <Route path='/checkout'>
+          <Route path="/checkout">
             <Header />
             <Checkout />
           </Route>
 
-          <Route path='/'>
+          <Route path="/">    
             <Header />
             <Home />
           </Route>
 
         </Switch>
+
       </Router>
+
     </div>
   );
 }
--- src/components/StateProvider/StateProvider.js
+++
@@ -1,3 +0,0 @@
-import React, { createContext, useContext, useReducer } from "react";
-
-
